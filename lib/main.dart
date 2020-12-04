12e3eaa117f83062a33e13fdf8060c14771ec36d
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-04 17:14:03 +0500

    Project has been updated

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,27 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: [
+          Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Image(
+                  image: AssetImage('images/dice1.png'),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Image(
+                  image: AssetImage('images/dice2.png'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
